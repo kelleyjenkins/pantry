fc8d84e76ba593b2f7376f0889958aea1813dc63
kelleyjenkins <ckjenkins3@gmail.com>
2017-11-06 13:14:26 -0700

    add pantry tests and methods

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,3 +1,20 @@
 class Pantry
+  attr_reader :stock
 
+  def initialize
+    @stock = {}
+  end
+
+  def add_item(item)
+    @stock[item] = 0
+  end
+
+  def stock_check(item)
+    @stock[item]
+  end
+
+  def restock(item, quantity)
+    #  @stock[item] = item
+    @stock[item] = quantity
+  end
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -4,4 +4,47 @@ require 'minitest/pride'
 
 class PantryTest < Minitest::Test
 
+  def test_it_exists
+    pantry = Pantry.new
+
+    assert_instance_of Pantry, pantry
+  end
+
+  def test_it_has_stock
+    pantry = Pantry.new
+
+    assert pantry.stock.empty?
+  end
+
+  def test_it_checks_stock_of_item
+    pantry = Pantry.new
+    pantry.add_item("Cheese")
+
+    assert_equal 0, pantry.stock_check("Cheese")
+  end
+
+  def test_it_can_restock
+    pantry = Pantry.new
+    pantry.add_item("Cheese")
+    pantry.restock("Cheese", 10)
+
+    assert_equal 10, pantry.stock_check("Cheese")
+  end
+
+  def test_it_can_add_quantity_to_restock
+    pantry = Pantry.new
+    pantry.add_item("Cheese")
+    pantry.restock("Cheese", 10)
+    pantry.restock("Cheese", 20)
+
+    assert_equal 30, pantry.stock_check("Cheese")
+  end
+
+
+
+
+
+  # def test_it_adds_quantity_to_stock_when_restocked
+
+
 end
